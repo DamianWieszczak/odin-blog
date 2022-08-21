0e1edede7a85ec6c0ddec31f96bf783b0e0a75d0
DamianWieszczak <damian.wieszczak@gmail.com>
2022-08-22 01:28:31 +0200

    Add deleting comments function

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
   include Visible
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true, length: { minimum: 3 }
   validates :body, presence: true, length: { maximum: 50 }
