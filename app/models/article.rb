366353145a921194304bbed71e547f29ed0c733a
DamianWieszczak <damian.wieszczak@gmail.com>
2022-08-22 01:19:24 +0200

    Add partials, status concern for comments and articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,9 @@
 class Article < ApplicationRecord
+  include Visible
+
   has_many :comments
 
   validates :title, presence: true, length: { minimum: 3 }
   validates :body, presence: true, length: { maximum: 50 }
+
 end
