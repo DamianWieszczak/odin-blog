88fbe64b6421382cbd84d9cc847acafc6cbcfde9
DamianWieszczak <damian.wieszczak@gmail.com>
2022-08-22 00:40:15 +0200

    Add comments feature

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+  has_many :comments
+
   validates :title, presence: true, length: { minimum: 3 }
   validates :body, presence: true, length: { maximum: 50 }
 end
