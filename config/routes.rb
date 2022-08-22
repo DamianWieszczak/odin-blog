02fc847d15374e9a05b9f061ea835686d5bd725f
DamianWieszczak <damian.wieszczak@gmail.com>
2022-08-22 20:15:15 +0200

    Add controllers from devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  devise_for :users
-  
+  devise_for :users, controllers: { registrations: "users/registrations" }
+
   resources :articles do
     resources :comments
   end
