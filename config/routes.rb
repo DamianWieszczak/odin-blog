9fd324a0e1678beaced57ec8032d03dbea79e16c
DamianWieszczak <damian.wieszczak@gmail.com>
2022-08-23 01:57:55 +0200

    Add navbar partial and message partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  devise_for :users, controllers: { registrations: "users/registrations" }
+  devise_for :users
 
   resources :articles do
     resources :comments
